9d6f88758a6f5ab42424255b5981a1f8521d150d
Cody Mickelsen <63523816+cody-mick@users.noreply.github.com>
2023-03-08 16:20:33 -0700

    👌 IMPROVE: Commiting to close the folder

--- src/components/character/CoreStat.tsx
+++ src/components/character/CoreStat.tsx
@@ -5,10 +5,21 @@ type CoreStat = { name: string; score: number; modifier: number; save: number };
 export default function CoreStat({ name, score, modifier, save }: CoreStat) {
 	return (
 		<div className="character-traits">
-			<div className="name">Name: {name}</div>
-			<div className="score">Score: {score}</div>
-			<div className="modifier">Modifier: {modifier}</div>
-			<div className="save">Save: {save}</div>
+			<div className="trait-name">{name}</div>
+			<div className="detail-blocks">
+				<div className="detail-block">
+					<div className="detail-block_title">Score</div>
+					<div className="detail-block_score">{score}</div>
+				</div>
+				<div className="detail-block">
+					<div className="detail-block_title">Modifier</div>
+					<div className="detail-block_score">{modifier}</div>
+				</div>
+				<div className="detail-block">
+					<div className="detail-block_title">Save</div>
+					<div className="detail-block_score">{save}</div>
+				</div>
+			</div>
 		</div>
 	);
 }
